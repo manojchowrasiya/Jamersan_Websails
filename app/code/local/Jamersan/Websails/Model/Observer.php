986f825897ee1435205c9c09ae81eed8070e4534
mbalparda <mbalparda@optaros.com>
2015-05-19 11:54:06 -0500

    Second commit. Added some features to the Observer.

--- app/code/local/Jamersan/Websails/Model/Observer.php
+++ app/code/local/Jamersan/Websails/Model/Observer.php
@@ -4,28 +4,33 @@ require_once("magmi_datapump.php");
 
 class Jamersan_Websails_Model_Observer {
     public function run() {
-// create a Product import Datapump using Magmi_DatapumpFactory
-        $dp=Magmi_DataPumpFactory::getDataPumpInstance("productimport");
+    	//initialize datapump
+    	$dp = Magmi_DataPumpFactory::getDataPumpInstance("productimport");
+		//open csv
+		$file = Mage::getModuleDir('', 'Jamersan_Websails') . DS . 'data' . DS .'import.csv';
+		$csv = new Varien_File_Csv();
+		$data = $csv->getData($file);
 
+		for($i=1; $i<count($data); $i++) {
+			$row = $data[$i];
+			
+			//see if product exist to define operation type
+			if (Mage::getModel('catalog/product')->load($row[5])->getName() != null) {
+				$action = 'update';
+			} else {
+				$action = 'create';
+			}
 
-        // Begin import session with a profile & running mode, here profile is "default" & running mode is "create".
-        // Available modes: "create" creates and updates items, "update" updates only, "xcreate creates only.
-        // Important: for values other than "default" profile has to be an existing magmi profile 
-        $dp->beginImportSession("default","create");
+			$dp->beginImportSession("default",$action);
 
-        //loop over 100 fake items
-        for($i=0;$i<100;$i++)
-        {
-         // Here we define a single "simple" item, with name, sku,price,attribute_set,store,description
-         // some varations on sku , name & description based on loop index
-         $testitem=array("name"=>"test item $i","sku"=>"SKU". str_pad((int) $i,4,"0",STR_PAD_LEFT),"price"=>"10.00","attribute_set"=>"Default","store"=>"admin","description"=>"ingested with Datapump API - item $i");
-
-         // Now ingest item into magento
-         $dp->ingest($testitem);
-        }
-
-
-        // End import Session
-        $dp->endImportSession();    
+	        //type, sku, name, config_attributes, price, weight, size, attr, visibility, qty, associated, super_attribute_pricing, manufacturer
+	         $testitem=array("type" => $row[3], "sku" => $row[5], "name" => $row[7], "config_attributes" => $row[16], "price" => $row[27], "weight" => $row[29], "size" => $row[31], "attr" => $row[32], "visibility" => $row[34], "qty" => $row[48], "manufacturer" => $row[88]);
+	         // Now ingest item into magento
+	         $dp->ingest($testitem);
+	        
+	        // End import Session
+	        $dp->endImportSession(); 
+	    }
+		 
     }
 }
